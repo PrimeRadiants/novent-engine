162b7a6b2508ac43052e7d3eb63c3180c1b1bdac
Shanira <georges.biaux@prime-radiants.com>
2016-08-30 20:54:39 +0200

    setting up npm & gulp project

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var del = require('del');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+gulp.task('default', ['compile']);
+gulp.task('watch', watch);
+
+gulp.task('clean', clean);
+gulp.task('compile', ['clean'], compile);
+
+function clean() {
+  return del(['dist/*']);
+}
+
+function compile() {
+  return gulp.src('src/*.js')
+    .pipe(concat('novent-engine.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+}
+
+function watch() {
+  gulp.watch('src/**/*.js', ['compile']);
+}
