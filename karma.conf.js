aa5bb79b2327557f56094461b5fabe14a6a0cf06
Shanira <georges.biaux@primeradiants.io>
2016-09-05 13:23:29 +0200

    adding event tests & activating code coverage

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'lib/**/*.js',
-      'dist/*.js',
+      'dist/**/*.js',
       'test/**/*.test.js'
     ],
 
@@ -29,13 +29,14 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'dist/**/*.js': 'coverage'
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
 
     // web server port
